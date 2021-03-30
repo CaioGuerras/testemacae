3b33f99a9dedd8cf67671674cb90d52f58d3edee
Caio Guerras <cguerras1001@gmail.com>
2021-03-29 22:49:19 -0300

    nova home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-return <img src="/avatar-big.png" title="Teste Macaé Top" alt="Teste Macaé Top"></img>
+return <img src="https://pbs.twimg.com/profile_images/1368580923571179523/sh3MpI6d_400x400.jpg" title="Teste Macaé Top" alt="Teste Macaé Top"></img>
 }
 
 export default Home
